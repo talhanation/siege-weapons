c9d1beecd86d0c63b458eb07b877e7058f989489
talhanation <talhakantar@yahoo.com>
2025-04-20 13:34:07 +0200

    Update SiegeWeapons.java

--- src/main/java/com/talhanation/siegeweapons/SiegeWeapons.java
+++ src/main/java/com/talhanation/siegeweapons/SiegeWeapons.java
@@ -1,34 +1,64 @@
 package com.talhanation.siegeweapons;
 
-import com.talhanation.siegeweapons.entities.AbstractVehicleEntity;
 import com.talhanation.siegeweapons.entities.BallistaEntity;
 import com.talhanation.siegeweapons.entities.CatapultEntity;
-import com.talhanation.siegeweapons.entities.TransportCartEntity;
 import com.talhanation.siegeweapons.init.ModEntityTypes;
+import com.talhanation.siegeweapons.init.ModItems;
 import com.talhanation.siegeweapons.world.SiegeTableRecipe;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
 
 public enum SiegeWeapons {
-    CATAPULT(0, new CatapultEntity(ModEntityTypes.CATAPULT.get(), Minecraft.getInstance().level), SiegeTableRecipe.CATAPULT_RECIPE),
-    //TRANSPORT_CART(1, new TransportCartEntity(ModEntityTypes.TRANSPORT_CART.get(), Minecraft.getInstance().level), SiegeTableRecipe.TRANSPORT_CART_RECIPE);
-    BALLISTA(1, new BallistaEntity(ModEntityTypes.BALLISTA.get(), Minecraft.getInstance().level), SiegeTableRecipe.BALLISTA_RECIPE);
+    CATAPULT(0, (level) -> new CatapultEntity(ModEntityTypes.CATAPULT.get(), level), SiegeTableRecipe.CATAPULT_RECIPE, 1.25F, 0, 0, 0),
+    BALLISTA(1, (level) -> new BallistaEntity(ModEntityTypes.BALLISTA.get(), level), SiegeTableRecipe.BALLISTA_RECIPE, 3.25F, 0, 0, 0),
+    BALLISTA_BOLT(2, (level) -> new ItemEntity(level, 0, 0, 0, ModItems.BALLISTA_PROJECTILE_ITEM.get().getDefaultInstance()), SiegeTableRecipe.BALLISTA_BOLT, 12F, 0, 15, 0),
+    COBBLE_CLUSTER(3, (level) -> new ItemEntity(level, 0, 0, 0, ModItems.COBBLE_CLUSTER_ITEM.get().getDefaultInstance()), SiegeTableRecipe.COBBLE_CLUSTER, 15F, 5, 15, 0),
+    FIRE_POT(4, (level) -> new ItemEntity(level, 0, 0, 0, ModItems.FIRE_POT_ITEM.get().getDefaultInstance()), SiegeTableRecipe.FIRE_POT, 15F, 5, 15, 0),
+    EXPLOSION_POT(5, (level) -> new ItemEntity(level, 0, 0, 0, ModItems.EXPLOSION_POT_ITEM.get().getDefaultInstance()), SiegeTableRecipe.EXPLOSION_POT, 15F, 5, 15, 0);
 
     private static final SiegeWeapons[] VALUES = values();
-    private final AbstractVehicleEntity entity;
+    private final Function<Level, Entity> entityFactory;
     private final int index;
+    private float renderScale;
+    private int renderXOffset;
+    private int renderYOffset;
+    private int renderZOffset;
     private final SiegeTableRecipe siegeTableRecipe;
-    SiegeWeapons(int index, AbstractVehicleEntity entity, SiegeTableRecipe siegeTableRecipe) {
+    SiegeWeapons(int index, Function<Level, Entity> entityFactory, SiegeTableRecipe siegeTableRecipe, float renderScale, int renderXOffset, int renderYOffset, int renderZOffset) {
         this.index = index;
-        this.entity = entity;
+        this.entityFactory = entityFactory;
         this.siegeTableRecipe = siegeTableRecipe;
+        this.renderScale = renderScale;
+        this.renderXOffset = renderXOffset;
+        this.renderYOffset = renderYOffset;
+        this.renderZOffset = renderZOffset;
     }
 
-    public AbstractVehicleEntity getEntity() {
-        return entity;
+    public Entity getEntity(Level level) {
+        return entityFactory.apply(level);
     }
 
     public int getIndex() {
         return index;
+    }
+
+    public float getRenderScale() {
+        return renderScale;
+    }
+    public int renderYOffset() {
+        return renderYOffset;
+    }
+
+    public int renderXOffset() {
+        return renderXOffset;
+    }
+    public int renderZOffset() {
+        return renderZOffset;
     }
 
     public SiegeTableRecipe getRecipe() {
@@ -41,5 +71,14 @@ public enum SiegeWeapons {
 
     public SiegeWeapons getBefore() {
         return VALUES[(this.ordinal() - 1 + VALUES.length) % VALUES.length];
+    }
+    @Nullable
+    public static SiegeWeapons fromIndex(int x){
+        for (SiegeWeapons weapon : SiegeWeapons.values()) {
+            if (weapon.getIndex() == x) {
+                return weapon;
+            }
+        }
+        return null;
     }
 }
