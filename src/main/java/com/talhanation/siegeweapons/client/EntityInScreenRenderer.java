815c3ea5909b8361e2404c3993b9b0ffef9ee057
talhanation <talhakantar@yahoo.com>
2025-04-20 13:34:12 +0200

    Update EntityInScreenRenderer.java

--- src/main/java/com/talhanation/siegeweapons/client/EntityInScreenRenderer.java
+++ src/main/java/com/talhanation/siegeweapons/client/EntityInScreenRenderer.java
@@ -11,7 +11,7 @@ import org.joml.Quaternionf;
 
 public class EntityInScreenRenderer {
 
-    public static void renderEntityInInventoryRotating(GuiGraphics p_282802_, int p_275688_, int p_275245_, int p_275535_, Entity entity, float rotationSpeed) {
+    public static void renderEntityInInventoryRotating(GuiGraphics p_282802_, int p_275688_, int p_275245_, int p_275535_, Entity entity, float rotationSpeed, float scale) {
         Minecraft mc = Minecraft.getInstance();
         float rotation = (mc.level != null ? mc.level.getGameTime() : mc.gui.getGuiTicks()) * rotationSpeed % 360;
 
@@ -19,14 +19,15 @@ public class EntityInScreenRenderer {
                 new Quaternionf()
                 .rotateX((float) Math.PI)
                 .rotateY(rotation * ((float) Math.PI / 180F));
-        renderEntityInInventory(p_282802_, p_275688_, p_275245_, p_275535_, rotationQuaternion, entity, rotation);
+        renderEntityInInventory(p_282802_, p_275688_, p_275245_, p_275535_, rotationQuaternion, entity, rotation, scale);
     }
 
-    public static void renderEntityInInventory(GuiGraphics p_282665_, int p_283622_, int p_283401_, int p_281360_, Quaternionf p_281880_, Entity entity, float rotation) {
-        p_282665_.pose().pushPose();
-        p_282665_.pose().translate((double) p_283622_, (double) p_283401_, 50.0D);
-        p_282665_.pose().mulPoseMatrix((new Matrix4f()).scaling((float) p_281360_, (float) p_281360_, (float) (-p_281360_)));
-        p_282665_.pose().mulPose(p_281880_);
+    public static void renderEntityInInventory(GuiGraphics guiGraphics, int p_283622_, int p_283401_, int p_281360_, Quaternionf p_281880_, Entity entity, float rotation, float scale) {
+        guiGraphics.pose().pushPose();
+        guiGraphics.pose().translate((double) p_283622_, (double) p_283401_, 50.0D);
+        guiGraphics.pose().mulPoseMatrix((new Matrix4f()).scaling((float) p_281360_, (float) p_281360_, (float) (-p_281360_)));
+        guiGraphics.pose().mulPose(p_281880_);
+        guiGraphics.pose().scale(scale,scale,scale);
         Lighting.setupForEntityInInventory();
 
         EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
@@ -36,13 +37,13 @@ public class EntityInScreenRenderer {
 
         entityrenderdispatcher.setRenderShadow(false);
         RenderSystem.runAsFancy(() -> {
-            entityrenderdispatcher.render(entity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, p_282665_.pose(), p_282665_.bufferSource(), 15728880);
+            entityrenderdispatcher.render(entity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, guiGraphics.pose(), guiGraphics.bufferSource(), 15728880);
         });
-        p_282665_.flush();
+        guiGraphics.flush();
         entityrenderdispatcher.setRenderShadow(true);
 
         entity.setYRot(prevYRot);
-        p_282665_.pose().popPose();
+        guiGraphics.pose().popPose();
         Lighting.setupFor3DItems();
     }
 
